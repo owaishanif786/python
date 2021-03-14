b86ed1043c658cbc7ad03e7933ffed86b1295490
owais <owaishanif786@gmail.com>
2021-03-15 02:24:00 +0500

    importing modules

---
+++ pcap3103/importing_modules/main.py
@@ -0,0 +1,12 @@
+import helpers as h
+from helpers import sum, square
+from helpers import sum as ss
+#from helpers import *
+
+print(f"Sum 5+4: {h.sum(5,4)}" )
+print(f"Sqauare 4: {h.square(4)}")
+
+print(f"Sum 5+5: {sum(5,5)}")
+print(f"Square 5: {square(5)}" )
+
+print(f"Sum 6+6 {ss(6,6)}")
