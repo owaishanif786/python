659900ff29625513c4c7c1f483afa0cda4d8ce1c
owais <owaishanif786@gmail.com>
2021-06-06 19:47:36 +0500

    composition

--- intro/classes.py
+++ intro/classes.py
@@ -3,15 +3,21 @@ class Car:
     Docstring describing car class
     """
 
-    def __init__(self, model, cc):
+    def __init__(self, model, cc, tires):
         self.model = model
         self.cc = cc
+        self.tires = tires
     
     def description(self):
-        print(f"{self.model} {self.cc}")
+        print(f"{self.model} {self.cc} {self.tires}")
 
+    def wheel_circumference(self):
+        if len(self.tires) > 0:
+            return self.tires[0].circumference()
+        else:
+            return 0
 
-if __name__ == '__main__':
-    c = Car('honda', 2000)
-    c.description()
+# if __name__ == '__main__':
+#     c = Car('honda', 2000, )
+#     c.description()
 
