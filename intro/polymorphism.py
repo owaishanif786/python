484a5e5797c0bf74bc3fae730551d8768417d9a4
owais <owaishanif786@gmail.com>
2021-06-06 22:17:28 +0500

    polymorphism or many forms an object can take

---
+++ intro/polymorphism.py
@@ -0,0 +1,11 @@
+#A parent can refference to any of child class.
+#i.e Person p = new Teacher() or Person p = new Student()
+#Here in above example now person reffernce can point to any type of its child object.
+
+#Let say SMS, Email and Push_Notification all inherits from Notification
+# so we can use 
+# Notification n = new SMS() or SMS s = new SMS()
+# Notification n = new Email or Email e = new Email()
+# Notification n = new Push_Notification() or PushNotification pn = new Push_Notification()
+
+
