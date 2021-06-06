df93394dff0996331355366d347f21eedde2b36b
owais <owaishanif786@gmail.com>
2021-06-07 01:08:42 +0500

    modules pip, venvs pipenv

---
+++ intro/modules.py
@@ -0,0 +1,50 @@
+#https://docs.python.org/3/py-modindex.html
+
+import math
+
+from math import pi, ceil
+from math import pi as p
+# from math import *
+
+#argparse, json, os, pdb
+
+#Thirdparty packages
+#pip3.7 install boto3 #installs in /lib
+#pip3.7 install boto3 --user #install in usr/lib
+#pip3.7 freeze #list all pinstall packages
+#pip3.7 list #more readable list
+#pip3.7 freeze > requirements.txt
+#pip3.7 install --user -r requirements.txt
+
+
+#installing package project basis when same packages but different versions required for different projects
+
+
+#mkdir ~/venvs
+#python3.7 -m venv ~/venvs/pg
+#source ~/venvs/pg/bin/activate
+#which python
+#pip install psycopg2
+#pip freeze
+#deactivate
+#which python
+#python -V
+
+
+
+#pipenv
+#pip3.7 install --user pipenv
+#cd ~/
+#mkdir database
+#cd database
+#pipenv --python python3.7
+# ls
+# cat Pipfile
+#pipenv shell
+#pipenv install pyscopg2
+#exit
+#pipenv shell
+
+
+
+
