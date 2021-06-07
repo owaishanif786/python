545f69e5ab711cb13e935d28422c824505286331
owais <owaishanif786@gmail.com>
2021-06-08 03:47:25 +0500

    implement s3 storage option

--- intro/projects/pgbackup/src/pgbackup/storage.py
+++ intro/projects/pgbackup/src/pgbackup/storage.py
@@ -2,3 +2,14 @@ def local(inflie, outfile):
     outfile.write(inflie.read())
     outfile.close()
     inflie.close()
+
+def s3(client, infile, bucket, name):
+    client.upload_fileobj(infile, bucket, name)
+
+# PYTHONPATH=./src python
+# import boto3
+# >>> from pgbackup import storage
+# >>> session = boto3.Session(profile_name='acg')
+# >>> client = session.client('s3')
+# >>> infile = open('example.txt', 'rb')
+# >>> storage.s3(client, infile, 'pg-backups-001', infile.name)
