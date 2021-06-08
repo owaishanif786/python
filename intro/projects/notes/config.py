74f288424af5621ec40cfe073c84de9f6864cede
owais <owaishanif786@gmail.com>
2021-06-08 18:57:12 +0500

    add user authentication

--- intro/projects/notes/config.py
+++ intro/projects/notes/config.py
@@ -1,10 +1,14 @@
 import os
+import pdb
 
+# pdb.set_trace()
 db_host = os.environ.get('DB_HOST', default='localhost')
 db_name = os.environ.get('DB_NAME', default='notes')
 db_user = os.environ.get('DB_USERNAME', default='notes')
 db_password = os.environ.get('DB_PASSWORD', default='')
 db_port = os.environ.get('DB_PORT', default=5432)
 
+# pdb.set_trace()
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+# SQLALCHEMY_DATABASE_URI = "postgresql://cloud_user:bingodingo@18.134.227.58/notes"
